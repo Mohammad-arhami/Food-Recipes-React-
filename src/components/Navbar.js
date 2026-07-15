c3eff79b01394ea3dcde946e2a665b739bb03844
Mohammad-Arhami <mohammad.arhami.20050902@gmail.com>
2026-07-15 15:05:33 +0330

    setup navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom'
+
 import React from 'react'
 import './Navbar.css'
 
 export default function NavBar() {
   return (
-    <div>
-      <h1>Food Recipes</h1>
+    <div className='navbar'>
+      <nav>
+        <Link to="/" className='brand'><h1>Food Recipes</h1></Link>
+        <Link to="/create">Crete Recipe</Link>
+      </nav>
     </div>
   )
 }
