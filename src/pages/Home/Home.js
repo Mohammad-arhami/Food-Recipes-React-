f4b79bcfd96b1715a3feab780fdcc613131fda11
Mohammad-Arhami <mohammad.arhami.20050902@gmail.com>
2026-07-15 15:10:58 +0330

    shoW all of the recipes in home route

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import RecipeList from '../../components/RecipeList';
+import {useFetch} from '../../hooks/useFetch';
 import './Home.css'
 
 export default function Home() {
+
+  const {data , isLoading , error} = useFetch('http://localhost:3000/recipes');
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isLoading && <p className='loading'>Loading...</p>}
+      {data && <RecipeList recipes={data}/>}
     </div>
   )
 }
