ced4e8bff2b5e626645c132e995f932b10743387
Mohammad-Arhami <mohammad.arhami.20050902@gmail.com>
2026-07-15 15:03:32 +0330

    recepi component (display single recipe With a specific ID )

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,10 +1,21 @@
+import { Link } from 'react-router-dom'
 import React from 'react'
 import './RecipeList.css'
 
-export default function RecipeList() {
+export default function RecipeList({recipes}) {
   return (
-    <div>
-      
+    <div className='recipe-list'>
+      {recipes.map(recipe => (
+        <div key={recipe.id} className='card'>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.cookingTime} To Make</p>
+            <div>{recipe.method.substring(0,100)}...</div>
+            <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+        </div>
+      ))}
     </div>
   )
 }
+// data.map((recipe) => (
+//         <h2 key={recipe.id}>{recipe.title}</h2>
+//       ))
