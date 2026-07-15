7eecc1bc07d913f0621126475313b0eb957119bc
Mohammad-Arhami <mohammad.arhami.20050902@gmail.com>
2026-07-15 15:14:52 +0330

    recepe rout  (display single recipe With a specific ID )

--- src/pages/Recipe/Recipe.js
+++ src/pages/Recipe/Recipe.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Recipe.css'
+import { useParams } from 'react-router-dom';
+import {useFetch} from '../../hooks/useFetch';
 
 export default function REcipe() {
+
+  const { id } = useParams();
+  const url = 'http://localhost:3000/recipes/'+id;
+  const {data : recipe , isLoading , error} = useFetch(url);
+
   return (
-    <div>
-      <h1>Recipe</h1>
+    <div className='recipe'>
+      {error && <p className='error'>{error}</p>}
+      {isLoading && <p className='loading'>Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className='page-title'>{recipe.title}</h2>
+          <p className='cooking-time'>Take {recipe.cookingTime} To Cook</p>
+          <ul>
+            {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+          </ul>
+          <p className='method'>{recipe.method}</p>
+        </>
+      )}
     </div>
   )
 }
