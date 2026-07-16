409de2feb8490744813c8e75a4eb62c76a50e708
Mohammad-Arhami <mohammad.arhami.20050902@gmail.com>
2026-07-16 22:12:52 +0330

    add new component called serachbar for searching into recipes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom'
 
 import React from 'react'
 import './Navbar.css'
+import Search from '../pages/Search/Search'
 
 export default function NavBar() {
   return (
     <div className='navbar'>
       <nav>
         <Link to="/" className='brand'><h1>Food Recipes</h1></Link>
+        <Search/>
         <Link to="/create">Crete Recipe</Link>
       </nav>
     </div>
---
+++ src/components/Searcbar.js
@@ -0,0 +1,29 @@
+import React , { useState } from 'react'
+import './Searchbar.css'
+import { useNavigate } from 'react-router-dom';
+
+export default function Searcbar() {
+
+  const [term , setTerm] = useState();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    navigate(`/search?q=${term}`)
+  } 
+
+
+  return (
+    <div className='searchbar'>
+      <form onSubmit={handleSubmit}>
+        <label>search:</label>
+        <input
+          id='search'
+          type="text"
+          onChange={(e) => setTerm(e.target.value)}
+          required
+        />
+      </form>
+    </div>
+  )
+}
