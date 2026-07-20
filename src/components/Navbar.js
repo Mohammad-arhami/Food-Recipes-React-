895395dcf0fe4852ab804ebf7fc212621708a5c6
Mohammad-Arhami <mohammad.arhami.20050902@gmail.com>
2026-07-20 22:17:25 +0330

    add searchbar to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom'
 
 import React from 'react'
 import './Navbar.css'
-import Search from '../pages/Search/Search'
+import Searchbar from './Searchbar'
 
 export default function NavBar() {
   return (
     <div className='navbar'>
       <nav>
         <Link to="/" className='brand'><h1>Food Recipes</h1></Link>
-        <Search/>
+        <Searchbar/>
         <Link to="/create">Crete Recipe</Link>
       </nav>
     </div>
