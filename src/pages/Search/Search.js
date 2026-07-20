b1a41205a7c0aed03e1b6ab0aa6379cc9a4c1157
Mohammad-Arhami <mohammad.arhami.20050902@gmail.com>
2026-07-20 22:35:24 +0330

    Filtering recipes that matched with the user's search query

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import './Search.css'
+import { useLocation } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch';
+import RecipeList from '../../components/RecipeList';
 
 export default function Search() {
 
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get('q');
+
+  const url = `http://localhost:3000/recipes`;
+  const {data ,isLoading, error} = useFetch(url);
+  
+  const recipes = data?.filter(recipe => {
+    const titlkeRecipe = recipe.title.toLowerCase().includes(query.toLowerCase());
+    return titlkeRecipe
+  })
+
   return (
-    <div >
-      search
+    <div>
+      <h2 className='page-title'>recipes Including "{query}"</h2>
+      {error && <p className='error'>{error}</p>}
+      {isLoading && <p className='loading'>Loading...</p>}
+      {recipes && <RecipeList recipes={recipes}/>}
     </div>
   )
 }
