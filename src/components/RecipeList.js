e525de9b8e5de69e13871d659f41e242b0e7263f
Mohammad-Arhami <mohammad.arhami.20050902@gmail.com>
2026-07-22 18:20:18 +0330

    defining new style for dark mode

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
 import './RecipeList.css'
+import { useThem } from '../hooks/useThem'
 
 export default function RecipeList({recipes}) {
+
+  const {mode} = useThem();
+
   return (
     <div className='recipe-list'>
       {recipes.map(recipe => (
-        <div key={recipe.id} className='card'>
+        <div key={recipe.id} className={`card ${mode}`}>
             <h3>{recipe.title}</h3>
             <p>{recipe.cookingTime} To Make</p>
             <div>{recipe.method.substring(0,100)}...</div>
