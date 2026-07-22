cb7226b3549fc1601c0a8902888d52dbaae442f9
Mohammad-Arhami <mohammad.arhami.20050902@gmail.com>
2026-07-22 18:21:45 +0330

    add dark and light mode icon in this component for select theme

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,14 +1,27 @@
 import { useThem } from "../hooks/useThem";
 import "./ThemeSelector.css";
+import modeIcon from '../assets/mode-icon.svg'
 
 const themeColors = ['#4e208a' , '#249c6b' , '#b70233'];
 
 export default function ThemeSelector() {
 
-    const {changeColor} = useThem()
+    const {changeColor , changeMode , mode} = useThem();
+
+    const toggleMode = () => {
+      changeMode(mode === 'dark' ? 'light' : 'dark' )
+    }
 
   return (
     <div className="theme-selector">
+      <div className="mode-toggle">
+        <img 
+          src={modeIcon}
+          onClick={toggleMode}
+          alt="light and dark icon"
+          style={{filter : mode === 'dark' ? 'invert(100%)' : 'invert(20%)'}}
+        />
+      </div>
       <div className="theme-buttons">
         {themeColors.map(color => (
             <div 
