3b5fdc78ce13d2badd8dbcfab3ef560ad35fe812
Mohammad-Arhami <mohammad.arhami.20050902@gmail.com>
2026-07-22 18:18:33 +0330

    add dark and light mode feature

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -7,6 +7,8 @@ const themeReducer = (state , action) => {
     switch (action.type) {
         case 'CHANGE_COLOR':
             return {...state , color: action.payload}
+        case 'CHANGE_MODE':
+            return {...state , mode: action.payload}
         default:
             return state
     }
@@ -16,15 +18,20 @@ const themeReducer = (state , action) => {
 export function ThemeProvider({children}) {
 
     const [state , dispatch] = useReducer(themeReducer , {
-        color : '#4e208a'
+        color : '#4e208a',
+        mode : 'dark'
     })
 
     const changeColor = (color) => {
         dispatch({type: "CHANGE_COLOR" , payload: color});
     }
 
+    const changeMode = (mode) => {
+        dispatch({type: "CHANGE_MODE" , payload: mode});
+    }
+
     return (
-        <ThemeContext.Provider value={ {color:{...state , changeColor}} }>
+        <ThemeContext.Provider value={ {color:{...state , changeColor , changeMode}} }>
             {children}
         </ThemeContext.Provider>
     )
